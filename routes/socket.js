6ff03d66744aeeafc2828c506361943114d551c3
void9main <void9main@163.com>
2017-03-25 15:06:35 +0800

    overload1.0

---
+++ routes/socket.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var io = require('socket.io');
+var net = require('net');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('socket');
+});
+
+module.exports = router;
