6467ef1d3a6c333502ee3c0ed8142aeac6ff5d90
void9main <void9main@163.com>
2017-03-25 14:39:36 +0800

    overload1.0

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'overload',content:'点击开始试用',des:'基于socket.io的即时通讯系统'});
 });
 
 module.exports = router;
