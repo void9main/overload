1c150a4ce75630edf0f35ef3619925fafdef9fe1
void9main <void9main@163.com>
2017-03-27 17:31:38 +0800

    overload1.0

--- routes/socket.js
+++ routes/socket.js
@@ -1,10 +1,21 @@
 var express = require('express');
+var app=express();
 var router = express.Router();
-var io = require('socket.io');
 var net = require('net');
-
+var http=require("http");
+var sio=require("socket.io");
+var server=http.createServer(app);
+var fs=require("fs");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('socket');
+  var socket=sio.listen(server);
+  socket.on("connection", function (socket) {
+    socket.emit("news",{hello:"你好"});
+    socket.on("otherEvent", function (data) {
+	    console.log(data);
+	  });
+  });
+  server.listen(3303);
 });
 module.exports = router;
